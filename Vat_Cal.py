1650c6ba293af417f6b16b78513a2e39b4b9fed8
Surawin <surawin92@gmail.com>
2023-07-11 16:53:11 +0700

    Make calculator
    There are many ways to solve a problem

---
+++ Vat_Cal.py
@@ -0,0 +1,9 @@
+print("Surawin")
+
+Price = float(input("Price = "))
+print("Prize with tax is ", x * 1.07)
+
+print(x + (x * 7/100))
+
+vat= 7/100
+print(Price + (Price * vat))
--- intro.py
+++ intro.py
@@ -1,2 +1,67 @@
-print("Surawin")
-print("Hello")
+# print("Surawin")
+
+# print("hello") 
+# print(123)
+# print(3.14)
+# print(2j + 8j)
+# print(True)
+# print(False)
+
+# '''DataType'''
+# print(type("Hello2"))
+# print(type(85))
+# print(type(9.12))
+# print(type(True))
+# '''
+# Test
+# '''
+# print(1+1)
+# print(8.5 + 1)
+# print("Hello" + "Saint")
+
+# '''operator +,-,/,*, **'''
+
+# print(3**2)
+# print(2 == 2)
+# print(2 != 2)
+
+
+# y = "Yaa"
+
+# x = int(input())
+# print(type(x))
+
+'''Calculator'''
+# Calculator
+
+# print("Put your desire number you want to add below")
+# x = float(input("Num1 = "))
+# y = float(input("Num2 = "))
+# print("Answer = ",x + y)
+
+# print("Put your desire number you want to subtract below")
+# x = float(input("Num1 = "))
+# y = float(input("Num2 = "))
+# print("Answer = ",x - y)
+
+# print("Put your desire number you want to multiply below")
+# x = float(input("Num1 = "))
+# y = float(input("Num2 = "))
+# print("Answer = ",x * y)
+
+# print("Put your desire number you want to divide below")
+# x = float(input("Num1 = "))
+# y = float(input("Num2 = "))
+# print("Answer = ",x / y)
+
+# x = int(input("first number"))
+# y = int(input("second number"))
+# print(x + y)
+
+#input name lastname
+name = input("Enter your first name: ")
+lastname = input("Enter your last name: ")
+print("Hello", name, lastname)
+
+
+
